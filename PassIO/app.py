f4e00fd200df9359da5b59e96597ed438e814794
JARED-WALDROFF <jared.waldroff@gmail.com>
2024-02-29 15:23:41 -0800

    Finished docker set up, got it consistently working with docker and able to use on localhost:5000, imported flask_pymongo set up for PyMongo set up. I also added 3 pages you can switch between for some basic functionality.

--- PassIO/app.py
+++ PassIO/app.py
@@ -1,25 +1,30 @@
 import time
 
 import redis
-from flask import Flask
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config['MONGO_URI'] = 'mongodb://localhost:5000/passio_db'
+mongo = PyMongo(app)
 app.debug = True
 cache = redis.Redis(host='redis', port=6379)
 
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
 
 @app.route('/')
-def hello():
-    count = get_hit_count()
-    return 'PassIO6! I have been seen {} times.\n'.format(count)
+def home():
+    return render_template('home.html')
 
+
+@app.route('/events')
+def events():
+    return render_template('events.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
