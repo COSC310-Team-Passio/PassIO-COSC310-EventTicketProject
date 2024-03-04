c7f6ab06cd48dbb371c5c3799426cee3165c4b9c
JARED-WALDROFF <jared.waldroff@gmail.com>
2024-03-03 19:10:53 -0800

    Added connection to atlas cloud DB. Removed link to local database in docker-compose.yml . Running the application in development mode so we can use debug. Added a dummy line to the home page in app.py so when you go to that page the data gets entered into the cloud database. Made a few entries in the database to test and everything works. Everyone in the group has a login and password, it's their name for both.

--- PassIO/app.py
+++ PassIO/app.py
@@ -1,18 +1,16 @@
-import time
-
 import redis
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb://localhost:5000/passio_db'
+app.config["MONGO_URI"] = "mongodb+srv://jared:jared@passioatlas.foiwof6.mongodb.net/passio_db?retryWrites=true&w=majority"
 mongo = PyMongo(app)
 app.debug = True
 cache = redis.Redis(host='redis', port=6379)
 
-
 @app.route('/')
 def home():
+    #mongo.db.host.insert_one({"name": "big venue", "address": "3424 big valley street, MA"})
     return render_template('home.html')
 
 
@@ -24,6 +22,7 @@ def events():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
 
 
 if __name__ == '__main__':
