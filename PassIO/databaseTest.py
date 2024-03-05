7691ae619eda273caffa0322155e7e5e84693672
AngryMagnets <liam.goel0678@gmail.com>
2024-03-05 15:13:25 -0800

    null is not a python keyword cool

--- PassIO/databaseTest.py
+++ PassIO/databaseTest.py
@@ -3,7 +3,7 @@ from pymongo.server_api import ServerApi
 
 def connect():
     username = "kaycee.goel.42%40gmail.com"
-    password = null
+    password = ""
     uri = "mongodb+srv://"+username+":"+password+"@passioatlas.foiwof6.mongodb.net/?retryWrites=true&w=majority"
     # Create a new client and connect to the server
     client = MongoClient(uri, server_api=ServerApi('1'))
