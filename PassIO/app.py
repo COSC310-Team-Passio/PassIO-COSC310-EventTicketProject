48bdbf3c1b7e0d92cf62c94a2a1b931a70088333
AngryMagnets <liam.goel0678@gmail.com>
2024-03-06 15:33:57 -0800

    Maybe some progress!
    I have no idea what I'm doing I can't even make a button that I can confirm does a thing. WHERE DO I SEE MY CONSOLE OUTPUT WHY DOES IT NOT JUST OUTPUT TO TERMINAL WHEN I CONNECT IT TO DOCKER ARRGGHGHHHHH

--- PassIO/app.py
+++ PassIO/app.py
@@ -1,6 +1,8 @@
+import time
 import redis
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
+from databaseTest import *
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb+srv://passio:passio@passioatlas.foiwof6.mongodb.net/passio_db?retryWrites=true&w=majority"
@@ -10,7 +12,7 @@ cache = redis.Redis(host='redis', port=6379)
 
 @app.route('/')
 def home():
-    #mongo.db.host.insert_one({"name": "biggest venue", "address": "3430 big valley street, MA"})
+    # mongo.db.host.insert_one({"name": "Venue for Ants", "address": "I know where you live"})
     return render_template('home.html')
 
 @app.route('/index')
@@ -25,12 +27,20 @@ def styleguide():
 def events():
     return render_template('events.html')
 
-
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+@app.route('/KayceeTest')
+def KayceeTest():
+    return render_template('KayceeTest.html')
 
+@app.route("/forward/", methods=["POST"])
+def move_forward():
+    print("Thing so I know I can do code stuff")
+    notification()
+    forward_message = "Moving Forward..."
+    return render_template("KayceeTest.html", forward_message=forward_message)
 
 if __name__ == '__main__':
     app.run(debug=True)
