0da548b8700b3d340b0ca1d514e79ebad97ec8cb
JARED-WALDROFF <jared.waldroff@gmail.com>
2024-03-08 11:58:10 -0800

    Updated checkout, eventEntry and index pages along with the css and scss files for style.

--- PassIO/app.py
+++ PassIO/app.py
@@ -38,5 +38,9 @@ def checkout():
 def admin():
     return render_template('admin.html')
 
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
