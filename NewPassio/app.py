e83d1695361ba7e8925ac0abeeca40bce0c52011
JARED-WALDROFF <jared.waldroff@gmail.com>
2024-03-07 18:35:07 -0800

    Everything works now, the style files work. My issue was that I had capitalized some of the folder names and that broke everything because its case-sensitive.

---
+++ NewPassio/app.py
@@ -0,0 +1,39 @@
+import redis
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb+srv://passio:passio@passioatlas.foiwof6.mongodb.net/passio_db?retryWrites=true&w=majority"
+mongo = PyMongo(app)
+app.debug = True
+cache = redis.Redis(host='redis', port=6379)
+
+@app.route('/')
+def index():
+    #mongo.db.host.insert_one({"name": "biggest venue", "address": "3430 big valley street, MA"})
+    return render_template('index.html')
+
+
+@app.route('/styleguide')
+def styleguide():
+    return render_template('styleguide.html')
+
+@app.route('/events')
+def events():
+    return render_template('events.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/checkout')
+def checkout():
+    return render_template('checkout.html')
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
