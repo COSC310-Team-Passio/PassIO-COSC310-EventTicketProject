07f1f370d86fb623ba3027edd185465855caece1
AngryMagnets <liam.goel0678@gmail.com>
2024-04-05 09:43:19 -0700

    Added the numTickets arg to the url of the checkout page call

--- PassIO/app.py
+++ PassIO/app.py
@@ -154,6 +154,8 @@ def register():
 
 @app.route('/checkout')
 def checkout():
+    numTickets = request.args.get('numTickets')
+    numTickets = min(max(numTickets, 1), 5) # Acts as Math.clamp would in other languages
     event_id = request.args.get('event_id')
     event_id = ObjectId(event_id)
     ticketQuery = {"event_id": event_id, "user_id": ""}
